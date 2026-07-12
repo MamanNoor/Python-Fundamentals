be6903461c280e6c89301fabd29f5fe94e3c47e2
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-12 06:15:28 -0700

    module-03 Arithmetic operators

---
+++ Module-03-Operators/arithmetic_operators.py
@@ -0,0 +1,11 @@
+num1 = 3
+num2 = 4
+
+print("Addition:",num1 + num2 )
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Remainder:",num1 % num2)
+print("Floor Division:", num1 // num2)
+print("Power(Exponent):", num1 ** num2)
+
