2218fced0d6a7962d200a67a7a20fb4cc406a961
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-15 01:04:19 -0700

    Comparison Operators Completed

---
+++ Module-03-Operators/comparison_operators.py
@@ -0,0 +1,9 @@
+num1 = 6
+num2 = 12
+
+print("Equal to:", num1 == num2)
+print("Less than:", num1 < num2)
+print("Greater than:", num1 > num2)
+print("Not Equal to:", num1 != num2)
+print("Less than or Equal to:", num1 <= num2)
+print("Greater than or Equal to:", num1 >= num2)
