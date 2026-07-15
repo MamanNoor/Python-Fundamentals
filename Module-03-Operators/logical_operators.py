02f853ac3a9ff20b670e8b5993fffe5ca3aaf2c6
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-15 01:51:20 -0700

    Logical Operators Completed

--- Module-03-Operators/logical_operators.py
+++ Module-03-Operators/logical_operators.py
@@ -4,6 +4,6 @@ num3 = 8
 num4 = 5
 num5 = 9
 
-print("AND Opertor:", num1 > num2 and num3 > num4)
+print("AND Operator:", num1 > num2 and num3 > num4)
 print("OR Operator:", num1 < num2 or num3 < num5)
-print("NOT Operator:", (not(num1 > num2),(num5 < num3)))
+print("NOT Operator:", not(num1 > num2),(num5 < num3))
