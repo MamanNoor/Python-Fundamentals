576e852ed4463bfce87c0b88427dc7b35841c8d0
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-17 10:22:37 -0700

    Membership operators completed

---
+++ Module-03-Operators/membership_operators.py
@@ -0,0 +1,8 @@
+word = "DEVELOPMENT"
+Languages_list = ["c++", "Java", "C", "C#", "Python"]
+
+print("in word:",("D" in word),("MEN" in word), ("VEL" in word))
+print("in languages:",("C" in Languages_list),("C#" in Languages_list))
+
+print("not in word:",("OEN" not in word),("DEV" not in word))
+print("not in Languages:",("Java" not in Languages_list),("HTML" not in Languages_list))
