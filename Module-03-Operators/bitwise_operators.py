40ee74260a2565066119dc5f143986bdf858ed15
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-17 10:56:00 -0700

    Bitwise Operators Completed

---
+++ Module-03-Operators/bitwise_operators.py
@@ -0,0 +1,9 @@
+num1 = 7
+num2 = 3
+
+print("Bitwise AND:", num1 & num2)
+print("Bitwise OR:", num1 | num2)
+print("Bitwise XOR:", num1 ^ num2)
+print("Bitwise NOT:", ~num1, ~num2)
+print("Bitwise shiftRight:", num1 >> num2)
+print("Bitwise shiftLeft:", num1 << num2)
