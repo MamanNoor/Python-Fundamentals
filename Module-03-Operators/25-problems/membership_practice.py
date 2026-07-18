9a3fe9848e88eb30d1a5f01cb1380a9614c6f5b6
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-18 06:43:42 -0700

    Membership Operators Practice

---
+++ Module-03-Operators/25-problems/membership_practice.py
@@ -0,0 +1,16 @@
+#list1
+fruits = ["Apple", "Banana", "Orange", "Mango"]
+search_fruit1 = "Banana"
+search_fruit2 = "Grapes"
+
+print("Banana in fruits:", search_fruit1 in fruits)
+print("Grapes in fruits:", search_fruit2 in fruits)
+print("Banana not in fruits:", search_fruit1 not in fruits)
+print("Grapes not in fruits:", search_fruit2 not in fruits)
+print()
+#list2
+Languages = ["C++", "Go", "Python", "Java"]
+print("Python in languages:", "Python" in Languages)
+print("Rust not in languages:", "Rust" not in Languages)
+print("C++ not in languages:", "C++" not in Languages)
+print("Go in languages:", "Go" in Languages)
