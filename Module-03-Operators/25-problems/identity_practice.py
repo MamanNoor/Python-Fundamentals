5b568657cc7f757d5210ccc6ccf79ccb75ee762c
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-18 02:59:52 -0700

    Identity Operators Practice

---
+++ Module-03-Operators/25-problems/identity_practice.py
@@ -0,0 +1,16 @@
+#Problem 5
+#Part 1
+list1 = [10, 20, 30]
+list2 = list1
+list3 = [10, 20, 30]
+
+print("list1 is list2:", list1 is list2)
+print("list1 is list3:", list1 is list3)
+print("list1 == list3:", list1 == list3)
+print("list1 is not list3:", list1 is not list3)
+#Part 2
+name1 = "Python"
+name2 = "Python"
+
+print("name1 == name2:", name1 == name2)
+print("name1 is name2:", name1 is name2)
