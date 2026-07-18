4adf3229afa260ffdb88c1c8f2c1b1b6f5fc8429
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-18 06:53:57 -0700

    Bitwise and operator precedence practice

---
+++ Module-03-Operators/25-problems/bitwise_practice.py
@@ -0,0 +1,10 @@
+a = 12
+b = 5
+
+print("Bitwise AND:",  a & b)
+print("Bitwise OR:",  a | b)
+print("Bitwise XOR:",  a ^ b)
+print("Bitwise NOT:",   ~b, ~a)
+print("Bitwise shift left:",  a << 1)
+print("Bitwise shift right:",  a >> 1)
+
---
+++ Module-03-Operators/25-problems/precedence_practice.py
@@ -0,0 +1,9 @@
+x = 10
+y = 5
+z = 2
+
+print("1:", x + y * z)
+print("2:", (x + y) * z)
+print("3:", x / z + y)
+print("4:", x // z + y)
+print("5:", x ** z + y)
