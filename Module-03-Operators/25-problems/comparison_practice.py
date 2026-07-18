744a1aac7eda436caddbfe86686b695e104501de
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-18 02:33:41 -0700

    Comparison Operators Practice

---
+++ Module-03-Operators/25-problems/comparison_practice.py
@@ -0,0 +1,9 @@
+age = 21
+required_age = 18
+
+print("Equal to:", age == required_age)
+print("Not Equal to:", age != required_age)
+print("Greater than:", age > required_age)
+print("Less than:", age < required_age)
+print("Greater than or equal to:", age >= required_age)
+print("Less than or equal to:", age <= required_age)
