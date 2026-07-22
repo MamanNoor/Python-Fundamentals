ad62046b151ff041015282b0c7960de7955887c2
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-22 01:41:04 -0700

    Complete Topic 03: Formatted Output

---
+++ Module-04-Input-Output/03_formatted_output.py
@@ -0,0 +1,15 @@
+#Heading 1 
+print("=" * 40)
+print("Student Information:".center(40))
+print("=" * 40)
+#User Input
+name = (input("Please enter your name:"))
+age = int(input("Please enter your age:"))
+city = (input("Please enter your city:"))
+#Print the output
+print()
+print("Name :", name)
+print("Age :", age)
+print("City :", city)
+#Final formatting
+print("=" * 40)
