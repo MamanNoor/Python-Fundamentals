c649b341143deb75ed0d094f95ff3bae0af7c047
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-22 05:24:38 -0700

    Complete Module 04 Theory: Input and Output

---
+++ Module-04-Input-Output/05_string_formatting.py
@@ -0,0 +1,11 @@
+name = (input("Please enter your name:"))
+age = int(input("Please enter your age:"))
+height = float(input("Please enter your height:"))
+
+# print("Hello {}!".format(name))
+# print("You are {} years old.".format(age))
+# print("Your height is {:.2f} meter.". format(height))
+ 
+print(f"Hello {name}!")
+print(f"You are {age} years old.")
+print(f"Your height is {height:.2f} meters.")
