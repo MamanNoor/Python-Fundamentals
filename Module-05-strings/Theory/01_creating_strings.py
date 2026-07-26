edca97cc063063b42e2a641f8661a11f243294be
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-26 17:01:54 +0500

    Creating Strings

---
+++ Module-05-strings/Theory/01_creating_strings.py
@@ -0,0 +1,21 @@
+# ========================================
+# Module 05 - Strings
+# Lesson 01 - Creating Strings
+# ========================================
+
+# Double Quotes
+name = "Noor"
+
+# Single Quotes
+city = 'Peshawar'
+
+# Triple Quotes
+message = """
+Welcome to Python Fundamentals.
+This is Module 05.
+Let's learn Strings!
+"""
+
+print(name)
+print(city)
+print(message)
