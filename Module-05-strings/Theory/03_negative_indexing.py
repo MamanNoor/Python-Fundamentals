5f6f2af24f5a1cc983b43f9cf469b2a25e700326
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-26 18:49:02 +0500

    Complete Module 05 Lesson 03 Negative Indexing

---
+++ Module-05-strings/Theory/03_negative_indexing.py
@@ -0,0 +1,22 @@
+#1st Heading
+print("-" * 40)
+print(" NEGATIVE INDEXING".center(40))
+print("-" * 40)
+#Variables
+name = "Noor"
+city = "Peshawar"
+country = "Pakistan"
+#Display Results
+print(f"Name    : {name}")
+print(f"City    : {city}")
+print(f"Country : {country}")
+print("-" * 40)
+
+#Negative Indexing
+print("NEGATIVE INDEXING". center(40))
+print(f"Last Character         : {name[-1]}")
+print(f"Second Last Character  : {name[-2]}")
+print(f"Third Last Character   : {name[-3]}")
+print(f"Fourth Last Character  : {name[-4]}")
+print("Negative indexing demonstrated successfully!")
+print("-" * 40)
