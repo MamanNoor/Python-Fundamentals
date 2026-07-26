2ddcef903467b6585e71304c9957476360389753
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-26 18:02:42 +0500

    Complete Module 05 Lessons 02

---
+++ Module-05-strings/Theory/02_indexing.py
@@ -0,0 +1,23 @@
+#1st Heading
+print("-" * 40)
+print("INDEXING".center(40))
+print("-" * 40)
+#Variables
+name = "Noor"
+quote = '"Start today, see results tomorrow"'
+message = """
+Welcome to Python Fundamentals
+Success comes with consistency """
+#Display Results
+print(f"Name    : {name}")
+print(f"Quote   : {quote}")
+print(f"Message : {message}")
+print("-" * 40)
+#String Indexing
+print(" STRING INDEXING ")
+print(f"First Character  : {name[0]}")
+print(f"Second Character : {name[1]}")
+print(f"Third Character  : {name[2]}")
+print(f"Last Character   : {name[3]}")
+
+print("=" * 40)
