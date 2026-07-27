1d569c6bbf1891fe5284c7cd5e72a90ddfd26236
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-27 18:09:08 +0500

    Complete Module 05 Lesson 05 String Immutability

---
+++ Module-05-strings/Theory/05_string_immutability.py
@@ -0,0 +1,10 @@
+name = "Noor"
+print("-" * 40)
+print("STRING IMMUTABILITY".center(40))
+print("-" * 40)
+new_name = "M" + name[1:]
+#Display Results
+print(f"Original Name  : {name}")
+print(f"New Name       : {new_name} ")
+print("String immutability demonstrated successfully!")
+print("-" * 40)
