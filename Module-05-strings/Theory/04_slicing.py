c3d257839e162360643e0234a24747d67d7491ee
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-07-27 11:00:56 +0500

    Complete Module 05 Lesson 04 String Slicing

---
+++ Module-05-strings/Theory/04_slicing.py
@@ -0,0 +1,23 @@
+#1st Heading
+print("-" * 40)
+print("STRING SLICING".center(40))
+print("-" * 40)
+#Variables
+name = "Noor"
+city = "Peshawar"
+country = "Pakistan"
+#Display results
+print(f"Name     : {name}")
+print(f"City     : {city}")
+print(f"Country : {country}")
+
+#2nd Heading/ Separator
+print("-" * 40)
+print("STRING SLICING".center(40))
+print("-" * 40)
+print(f"First Four Letters  : {country[0:4]}")
+print(f"Middle Four Letters : {country[2:6]}")
+print(f"Last Three Letters  : {country[5:8]}")
+print(f"Complete Word       : {country}")
+print("String slicing demonstrated successfully!")
+print("-" * 40)
