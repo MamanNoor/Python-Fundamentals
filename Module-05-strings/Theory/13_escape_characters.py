14c9c6c30f45d013e600faf1a7ee7d4333619f85
Mamoona Noor <mamoonaa.noor@gmail.com>
2026-08-02 12:26:30 +0500

    Demonstrate Python escape characters

---
+++ Module-05-strings/Theory/13_escape_characters.py
@@ -0,0 +1,51 @@
+#Escape Characaters
+print("=" * 40)
+print("ESCAPE CHARACTERS".center(40))
+print("=" * 40)
+#Variable
+name = "Noor"
+course = "Python"
+path = "C:\\Users\\Noor\\Desktop"
+quote = "Practice every day."
+#Display Original Values
+print(f"Name   :  {name}")
+print(f"Course :  {course}")
+print(f"Path   :  {path}")
+print(f"Quote  :  {quote}")
+print()
+#Separator 1
+print("-" * 40)
+print("New Line(\\n)".center(40))
+print("-" * 40)
+print(f"Name  : {name}\nCourse  : {course}\nStatus  : Learning")
+print()
+#Separator 2
+print("-" * 40)
+print("Tab(\\t)".center(40))
+print("-" * 40)
+print("Name\tCourse")
+print(f"{name}\t{course}")
+print()
+#Separator 3
+print("-" * 40)
+print("Backslash(\\\\)".center(40))
+print("-" * 40)
+print(path)
+print()
+#Separator 4
+print("-" * 40)
+print("Double Quotes(\\\")".center(40))
+print("-" * 40)
+print(f'The teacher said, "{quote}"')
+print()
+#Separator 5
+print("-" * 40)
+print("Single Quote(\\')".center(40))
+print("-" * 40)
+print('Don\'t stop learning Python.')
+print()
+#Success Message
+print("Escape Characters demonstrated successfully!")
+print("=" * 40)
+
+ 
